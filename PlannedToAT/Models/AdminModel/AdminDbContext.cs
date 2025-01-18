6362d941d51cfec40ece6b11e5209cf8d0467872
Moseslwa <Moseslwanga30@gmail.com>
2025-01-17 20:01:39 -0500

    Changes to be committed: 	modified:   PlannedToAT/Models/AdminModel/AdminDbContext.cs

--- PlannedToAT/Models/AdminModel/AdminDbContext.cs
+++ PlannedToAT/Models/AdminModel/AdminDbContext.cs
@@ -8,8 +8,8 @@ namespace PlannedToAT.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseMySQL("server=localhost;database=plannedtoat;user=root;password=Moses.123");
-            optionsBuilder.UseMySQL("server=plannedtoatscus.mysql.database.azure.com;database=plannedtoat;user=Jamy;password=Faye2011");
+            optionsBuilder.UseMySQL("server=localhost;database=plannedtoat;user=root;password=Moses.123");
+            //optionsBuilder.UseMySQL("server=plannedtoatscus.mysql.database.azure.com;database=plannedtoat;user=Jamy;password=Faye2011");
         }
     }
 }
